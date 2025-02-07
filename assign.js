1ed806bc7a5f1cf0bc60866ad35523de505fa050
deepthi mekala <mekaladeepthi83@gmail.com>
2025-02-06 23:10:45 -0500

    task1

---
+++ assign.js
@@ -0,0 +1,22 @@
+//   let n1 = Number(prompt("Enter first number: "));
+//   let n2 = Number(prompt('Enter second number: '));
+    
+//    let sum = n1 + n2;
+    
+//    window.alert(`Addition result is ${sum}`);
+//    console.log(sum);
+//    document.write(sum);
+
+//    let age = prompt('please enter your age',25);
+//    alert(`you are ${age} year old`)
+
+
+//    let userchoice = confirm('are you sure, you want to delete the record??');
+//    alert(`user is ${userchoice ? `willing` :'not willing'} To delete`);
+
+   let msg = 'Helllo, Good Morning!!!';
+   console.log(msg);
+//    console.warn(msg);
+//    console.error(msg);
+   
+  
