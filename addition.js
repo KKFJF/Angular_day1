f3a82a8c911d0f1b00ba5dfbc489d9b9b792806f
Akshitha Reddy Maryadha <dotnet01_pa@g.com>
2025-02-07 04:21:31 +0000

    Upload New File

---
+++ addition.js
@@ -0,0 +1,11 @@
+let a = Number(prompt("Enter 1st number"));
+let b = Number(prompt("Enter 2nd number"));
+let result = (a + b);
+
+document.write(`sum of ${a} and ${b} is ${result}`);
+window.alert(`Sum of ${a} and ${b} is ${result}`);
+console.log(result);
+
+let age = Number(prompt("Enter your age"));
+window.alert((`You are ${age} years old !!!`));
+
